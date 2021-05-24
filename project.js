80f00d5cb521505fd10fcb7f1bd6ca2b9ac0cda2
Aaron Byl <aaron.byl@gmail.com>
2021-05-24 09:25:25 -0400

    Home to Index

--- project.js
+++ project.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/',function(req,res){
-  res.render('home');
+  res.render('index');
 });
 
 app.get('/subpage1',function(req,res){
